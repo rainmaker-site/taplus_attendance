ffd8fade5a86ff3ec1acdd84a4534c1fd695665b
yoshinobu.inoue <ifactory@yahoo.co.jp>
2020-02-23 20:36:44 +0900

    詳細画面の簡易実装

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -4,16 +4,16 @@ class AttendanceController < ApplicationController
     end
 
     def detail
-        @employee = MstEmployee.find(params[:employee_id])
+        @employee = MstEmployees.find(params[:employee_id])
 
-        @hours = MstCodeDetails.where(code_headers_id: 1)
-        @minutes = MstCodeDetails.where(code_headers_id: 2)
+        @hours = MstCodeDetails.where(code_headers_id: 1).pluck(:detail_name)
+        @minutes = MstCodeDetails.where(code_headers_id: 2).pluck(:detail_name)
 
         now = Date.today
         @year = now.year
         @month = now.month
         @dt = Date.new(now.year, now.month, 1)
-        @working_hours = TrnWokingHours.where(employee_id: params[:employee_id])
+        @working_hours = TrnWorkingHours.where(employee_id: params[:employee_id])
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'attendance/index'
-  get 'attendance/detail/:employee_id'
+  #get 'attendance/index'
+  get 'attendance/detail/:employee_id', to: 'attendance#detail'
 
   root 'attendance#index'
 end
